661ee0302b6a6e9b52a02fd8da7b371bd2522cf2
hammadNS96 <hammad96@outlook.com>
2020-09-15 10:36:56 -0400

    uploading from a new computer

---
+++ numbers.js
@@ -0,0 +1,6 @@
+let num = 2
+let neer = 56
+
+let result = num + neer;
+
+console.log(result)
---
+++ strings.js
@@ -0,0 +1,4 @@
+let city = " Toronto"
+let country = " Canada"
+let location = " Granrock Crescent "
+console.log(city + country + location)
